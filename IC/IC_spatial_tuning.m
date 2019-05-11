86dcded2d5749d4415a1a83b9857197efe1082a1
kfchou <kfchou@bu.edu>
2019-05-10 20:45:25 -0400

    added plotting script

--- IC/IC_spatial_tuning.m
+++ IC/IC_spatial_tuning.m
@@ -52,6 +52,12 @@ for i = 1:nPositions
     randomness = 1;
     azList = [-90,-60,-45,-30,0,30,45,60,90]; %model neuron locations
     [spks(i).spk_IC, spks(i).FR] = ICmodel(s_filt,azList,randomness);
+    
+    saveLoc = 'J:\Data\spatial tuning single wgn';
+    spk_IC = spks(i).spk_IC;
+    FR = spks(i).FR;
+    saveName = sprintf('Spatial Tuning SingleWGN Position %02i.mat',i);
+    save([saveLoc filesep saveName],'spk_IC','FR','sourcePositions','azList');
     toc
 end
 disp('done')
@@ -65,6 +71,8 @@ disp('done')
 % %     save([saveLoc saveName],'spk_IC','FR','sourcePositions','azList');
 % % end
 %% activity = number of total spikes
+addpath(genpath('C:\Users\Kenny\Dropbox\Sen Lab\m-toolboxes\plotting utils'))
+
 for j = 1:length(azList)
     for i = 1:nPositions
         numSpk(i,j) = sum(sum(spks(i).spk_IC(:,:,j)));
@@ -75,23 +83,32 @@ plot(sourcePositions,numSpk,'linewidth',2);
 ylabel('spike count (all freq channels)')
 xlabel('azimuth')
 title('total spike count v noise location')
-legend(cellstr(num2str(azList')))
+hleg = legend(cellstr(num2str(azList')));
+hleg.Title.String = ['Neuron' newline 'Preferred' newline 'Location'];
+hleg.EdgeColor = [1 1 1];
+set(gca,'FontSize',12)
+cmap = cubehelix(length(azList),1.16,2.56,1.93,1.1,[.29,.68],[.21,.83]);
+hline = findobj(gcf,'type','line');
+for i = 1:length(hline)
+    hline(i).Color = cmap(i,:);
+    hline(i).LineWidth = 3;
+end
 
 %% firing rates (1)
-figure;
-for j = 1:length(azList)
-    for i = 1:nPositions
-        spkPerChan(i,:) = sum(spks(i).spk_IC(:,:,j));
-    end
-    figure;
-    subplot(3,3,j)
-    imagesc(sourcePositions,cf,spkPerChan')
-    ylabel('freq channel CFs')
-    xlabel('noise source azimuth')
-    set(gca,'ydir','normal')
-    title(sprintf('%i deg az neurons',azList(j)))
-end
-suptitle('total firing activity per frequency channel')
+% figure;
+% for j = 1:length(azList)
+%     for i = 1:nPositions
+%         spkPerChan(i,:) = sum(spks(i).spk_IC(:,:,j));
+%     end
+% %     figure;
+%     subplot(3,3,j)
+%     imagesc(sourcePositions,cf,spkPerChan')
+%     ylabel('freq channel CFs')
+%     xlabel('noise source azimuth')
+%     set(gca,'ydir','normal')
+%     title(sprintf('%i deg az neurons',azList(j)))
+% end
+% suptitle('total firing activity per frequency channel')
 
 %% firing rates (2) - same as (1)
 % figure;
@@ -131,3 +148,15 @@ suptitle('total firing activity per frequency channel')
 % xlabel('noise source azimuth')
 % set(gca,'ydir','normal')
 % title(sprintf('%i deg az neurons',azList(j)))
+
+%%
+% addpath('C:\Users\Kenny\Desktop\GitHub\SpatialAttentionNetwork\dependencies')
+% figure;
+% for i = 1:9
+%     subplot(3,3,i)
+%     icSpikes = logical(squeeze(spks.spk_IC(:,:,i))'); 
+%     plotSpikeRasterFs(icSpikes, 'PlotType','vertline', 'Fs',40000);
+%     xlim([0 2000])
+%     if i==1, ylabel('IC spikes'); end
+%     set(gca,'Ytick',[1:64],'YtickLabel',cf)
+% end
