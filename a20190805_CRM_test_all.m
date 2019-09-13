fb90bb2f3baedd636581f0c13b9d0f3c042097d4
kfchou <kfchou@bu.edu>
2019-09-13 15:47:19 -0400

    more scripts

--- a20190805_CRM_test_all.m
+++ a20190805_CRM_test_all.m
@@ -1,23 +1,36 @@
 % signal through encoder, process via network
 % 20190411 - works with variable spatial tuning parameters, for modeling
 %            neurons with different spatial preferences
+% 20190821 - this file works on the SCC
 
-cd('C:\Users\Kenny\Desktop\GitHub\PISPA2.0')
-addpath('Peripheral');
-addpath('hrtf');
-addpath('IC');
-experiment_name = '009 2masker example for BOSS';
-dataDir = 'Z:\eng_research_hrc_binauralhearinglab\kfchou\ActiveProjects\CISPA2.0';
+% --- paths for linux based systems ---
+% addpath('~/BOSS_algo_psychoacoustics/Peripheral');
+% addpath('HRTF_40k');
+% addpath('~/BOSS_algo_psychoacoustics/IC');
+% addpath('~/BOSS_algo_psychoacoustics/Recon')
+% experiment_name = 'CRM_SymmetricalMaskers';
+
+% --- paths for windows based systems ---
+addpath('../Peripheral');
+addpath('C:\Users\Kenny\Desktop\GitHub\PISPA2.0\HRTF');
+addpath('../IC');
+addpath('../Recon')
+experiment_name = 'CRM_SymmetricalMaskers';
 
 %======================== set parameters =======================
+dataDir = 'SimDebug';
 
 % Stimulus parameters
 % Each cell element is a trial
-speakerIdxs = 6:20;
+speakerIdxs = 1:20;
 % speakerIdxs = {{'030105'}};
 talkers = 4;
 % azs = {[0],[90], [0 90]};
-azs = {[0,90,-90]};
+% azs = {[0,90,-90]};
+m1pos = 90:-5:-90;
+m2pos = -90:5:90;
+tpos = zeros(length(m1pos),1);
+azs = num2cell([tpos m1pos' m2pos'],2)';
 % azs = num2cell(zeros(1,20)); %for-loop vector must be horizontal. Fun fact.
 % azs = num2cell(-90:10:90);
 % azs = num2cell([90]);
@@ -38,7 +51,7 @@ freqGainNorm = 0;
 
 %==============================================================
 
-dataLoc = sprintf('%s\\Data\\%s %s\\%s\\',dataDir,experiment_name,IC_param,experiment_setup);
+dataLoc = sprintf('%s/Data/%s %s/%s/',dataDir,experiment_name,IC_param,experiment_setup);
 if ~exist(dataLoc,'dir'), mkdir(dataLoc); end
 
 %make backup of current script
@@ -51,10 +64,11 @@ copyfile(currentfile,newbackup);
 [nf,cf,bw] = getFreqChanInfo('erb',numChannel,low_freq,high_freq);
 fcoefs=MakeERBFilters(fs,cf,low_freq);
 
-for az_cell = azs
-    az = cell2mat(az_cell);
+parfor ii = 1:length(azs)
+    az = azs{ii};
     
     for speakerIdx = speakerIdxs
+        disp(['currently procesing idx: ' num2str(speakerIdx)])
         if iscell(speakerIdx)
             if length(az) ~= length(speakerIdx{1}), error('undefined configuration');end
             binnum = str2num(cell2mat(speakerIdx{1}'))';
@@ -69,7 +83,8 @@ for az_cell = azs
         audiowrite(sprintf('%s%s_mixed.wav',dataLoc,trial_id),mixed,fs);
 
         %reference signals = original filtered signal = target in STOI
-        clear refC ref
+        ref = [];
+        refC = [];
         speaker_id = {'target.wav','masker1.wav','masker2.wav'};
         speaker_conv_id = {'target_conv.wav','masker1_conv.wav','masker2_conv.wav'};
         for i = 1:length(s_original)
@@ -85,6 +100,7 @@ for az_cell = azs
 
         % Peripheral model
         % plot_filterbankSupport;
+        s_filt = struct();
         s_filt.sL=ERBFilterBank(s_mixed.sL,fcoefs); %freq*time
         s_filt.sR=ERBFilterBank(s_mixed.sR,fcoefs);
         s_filt.band = 'narrow';
@@ -96,6 +112,7 @@ for az_cell = azs
         s_filt.highFreq = high_freq;
 
         %frequency gain normalization across channels
+        frgain = 1;
         if freqGainNorm
             temp = mean(abs(s_filt.sL),2); %mean of each freq channel
             frgain = 1./(temp/max(temp));
@@ -111,36 +128,47 @@ for az_cell = azs
         tic
         [spk_IC, firingrate] = ICmodel(s_filt,azList,randomness);
         toc
-        save(sprintf('%s%s_SpkIC.mat',dataLoc,trial_id),'spk_IC','freqGainNorm','input_gain','cf','fcoefs');
+        mysaveSpks(sprintf('%s%s_SpkIC.mat',dataLoc,trial_id),spk_IC,freqGainNorm,input_gain,cf,fcoefs);
         
+        % mask calculation
+        spks = spkTime2Train(spk_IC,fs,length(mixed));
+        maskParam = struct();
+        maskParam.kernel = 'alpha';
+        maskParam.tau = 0.02;
+        maskParam.delay = 0;
+        maskParam.maskRatio = 0;
+        masks = calcSpkMask(spks,fs,maskParam);
+        masksNorm = zeros(size(masks));
+        if ndims(masks) == 3
+            % normalize masks
+            for i = 1:size(masks,3)
+                masksNorm(:,:,i) = masks(:,:,i)/max(max(masks(:,:,i)));
+            end
+
+            % remove side channels
+            centerM = masksNorm(:,:,3);
+            rightM1 = masksNorm(:,:,5);
+            rightM2 = masksNorm(:,:,4);
+            leftM1 = masksNorm(:,:,1);
+            leftM2 = masksNorm(:,:,2);
+            spkMask = centerM-maskParam.maskRatio.*(rightM1+leftM1+leftM2+rightM2);
+            spkMask(spkMask<0)=0; %half wave rectify
+        else
+            spkMask = masks;
+        end
+        spkMask = spkMask./max(max(abs(spkMask))); %normalize to [0,1]
+        
+        % Reconstruction
+        [rstim1dual, ~] = applyMask(spkMask,s_filt.sL,s_filt.sR,frgain,'filt');
+        audiowrite(sprintf('%s%s_recon.wav',dataLoc,trial_id),rstim1dual/max(max(rstim1dual))*.99,fs)
+        
+        % Calculate scores
+        st = zeros(1,3);
+        for jj = 1:3
+            st(jj) = runStoi(rstim1dual,s_original(jj).wav,fs,fs);
+        end
+        stFilename = sprintf('%s%s_stoi.mat',dataLoc,trial_id);
+        mysaveSTOI(stFilename,st);
     end
 end
-
-%%
-% addpath('recon')
-% % temp = vocode(firingrate(:,:,3)',cf,'tone');
-% locs = [-90:45:90];
-% figure;
-% for i = 1:5
-%     subplot(2,3,i)
-%     mask = calcSpkMask(spk_IC(:,:,i),fs,'alpha',0.01);
-%     taxis = 0:1/fs:length(mixed)/fs-1/fs;
-%     imagesc(taxis,cf,mask); set(gca,'ydir','normal')
-%     xlabel('time')
-%     ylabel('frequency')
-%     title([num2str(locs(i)) 'deg cell']);
-% end
-% % temp2 = vocode(mask(:,:,3),cf,'tone',fs);
-% % temp3 = vocode(mask(:,:,5),cf,'tone',fs);
-
-%% plot spike rasters
-% addpath('C:\Users\Kenny\Desktop\GitHub\SpatialAttentionNetwork\dependencies')
-% figure;
-% for i = 1:5
-%     subplot(1,5,i)
-%     icSpikes = logical(squeeze(spk_IC(:,:,i))'); 
-%     plotSpikeRasterFs(icSpikes, 'PlotType','vertline', 'Fs',40000);
-%     xlim([0 2000])
-%     if i==1, ylabel('IC spikes'); end
-%     set(gca,'Ytick',[1:64],'YtickLabel',cf)
-% end
+disp('job finished')
