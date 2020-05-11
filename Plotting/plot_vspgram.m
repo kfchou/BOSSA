23d221a2d1e34a9bbfac9f496a1aa08e9e163d61
kfchou <kfchou@bu.edu>
2020-05-11 18:02:52 -0400

    add vspgram plotting function

---
+++ Plotting/plot_vspgram.m
@@ -0,0 +1,32 @@
+function [h, Braw] = plot_vspgram(waveform,fs,p)
+%% mask to highlight f0 and harmonics, using the smoothed f0
+% returns:
+%   h = figure handle
+
+if ~exist('p','var'); p = struct(); end
+if ~isfield(p,'fstep'); p.fstep=5; end      % frequency resolution of initial spectrogram (Hz)
+if ~isfield(p,'tinc'); p.tinc = 0.01; end   % maximum frequency of initial spectrogram (Hz)
+if ~isfield(p,'fmax'); p.fmax=8000; end     % default frame increment (s)
+if ~isfield(p,'fmin'); p.fmin=0; end     % default frame increment (s)
+
+
+% spectrogram       
+fres = 20; %bandwidth, from fxpefac
+fmin = p.fmin;
+faxis_spec = [fmin p.fstep p.fmax];
+[tx,F,B,~,Brawfilt,spec] = spgrambw(waveform,fs,fres,faxis_spec,[],p.tinc); %run this line to extract some parameters
+
+% Voicebox Spectrogram
+[sf,~]=enframe(waveform,spec.win,spec.ninc);
+frameLen = length(spec.win);
+Braw=rfft(sf,frameLen,2);
+Braw = Braw';
+Bmag = abs(Braw);
+% h = figure;
+fxraw = [0:frameLen-1]/frameLen*fs;
+fxraw = fxraw(1:frameLen/2);
+imagesc(tx,fxraw,20*log(Bmag));
+set(gca,'ydir','normal')
+ylim([0,8000])
+title('magnitude')
+hold on;
